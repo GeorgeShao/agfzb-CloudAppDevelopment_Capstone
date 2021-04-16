3e784ba5a2d8c1bc804900b9420974bf78ee00ca
George Shao <georgeshao123@gmail.com>
2021-04-16 18:42:19 -0400

    week 3 - added models & api

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -3,19 +3,18 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render, redirect
 # from .models import related models
-# from .restapis import related methods
+from .restapis import get_dealers_from_cf, get_dealer_by_id_from_cf, get_dealer_reviews_from_cf
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 from datetime import datetime
 import logging
 import json
 
+
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
 
-
 # Create your views here.
-
 
 # Create an `about` view to render a static about page
 def about(request):
@@ -84,14 +83,22 @@ def registration_request(request):
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
+    logger.warning("TEST 123")
     context = {}
     if request.method == "GET":
+        dealerships = get_dealers_from_cf("https://af176ef2.us-south.apigw.appdomain.cloud/api/dealership/")
+        context['dealer_list'] = dealerships
         return render(request, 'djangoapp/index.html', context)
 
 
 # Create a `get_dealer_details` view to render the reviews of a dealer
-# def get_dealer_details(request, dealer_id):
-# ...
+def get_dealer_details(request, dealer_id):
+    context = {}
+    if request.method == "GET":
+        # Get reviews from the URL
+        reviews = get_dealer_reviews_from_cf("https://af176ef2.us-south.apigw.appdomain.cloud/api/review/", dealer_id)
+        context['review_list'] = reviews
+        return render(request, 'djangoapp/dealer_details.html', context)
 
 # Create a `add_review` view to submit a review
 # def add_review(request, dealer_id):
